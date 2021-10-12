bb5d4079436d0c8b704093bd19925f28238f7cfa
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-10-12 10:55:30 +0200

    Create Navbar and add it to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import '../styling/App.css';
+import { Provider } from 'redux';
+import store from '../Redux/storeConfig';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Navbar />
+        <h2>All countries</h2>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div>
+    <h2>THis is navbar</h2>
+    <p> Navigation</p>
+  </div>
+);
+
+export default Navbar;
