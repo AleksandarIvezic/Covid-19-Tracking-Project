85ad0ca5eb081958e1e13e6cf1918b52c6112775
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-10-14 22:19:14 +0200

    Refactor country unit test

--- src/__tests__/componentsTest/Country.test.js
+++ src/__tests__/componentsTest/Country.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import store from '../../redux/storeConfig';
 import Country from '../../components/Country';
 
 describe('Country', () => {
   it('renders correctly', () => {
     const tree = renderer
-      .create(<Provider store={store}><Country country="Serbia" totalConfirmed={7} handleClick={() => 2 + 3} /></Provider>)
+      .create(<Country country="Serbia" totalConfirmed={7} handleClick={() => 2 + 3} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
