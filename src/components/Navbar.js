d8e0bf17cded2b8959a2f9f64a5b8724435f5fe2
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-10-14 17:15:38 +0200

    Style Navbar and Search

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,14 @@ const Navbar = () => {
   const location = useLocation();
   return (
     <nav className="d-flex justify-content-between p-1 sticky-top">
-      {(location.pathname !== '/') ? <button className="back" type="button" onClick={history.goBack}><i className="fas fa-chevron-left text-light"> </i></button> : <span className="fw-bold text-uppercase">Countries</span>}
+      {(location.pathname !== '/') ? <button className="back" type="button" onClick={history.goBack}><i className="fas fa-chevron-left text-light"> </i></button> : <span className="fw-bold text-uppercase back">Countries</span>}
       <span className="fw-light fs-6 text-lowercase">
         Track COVID-19
         {location.pathname}
       </span>
       <div>
-        <i className="fas fa-microphone mx-1" />
-        <i className="fas fa-cog mx-1" />
+        <i className="fas fa-microphone mx-2" />
+        <i className="fas fa-cog mx-2" />
       </div>
     </nav>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ import '../styling/Search.css';
 const Search = ({ handleChange }) => (
   <form className="d-flex stripe-text justify-content-between">
     <span className="m-1">STATS BY COUNTRY</span>
-    <input name="search" type="text" placeholder="Search" onChange={handleChange} />
+    <input className="search" name="search" type="text" placeholder="Search" onChange={handleChange} />
   </form>
 );
 
